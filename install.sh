6b0424337638015abeac0f66488bb6d4f10b993e
bmstefanski <bstefanskicom@gmail.com>
2018-05-07 22:31:24 +0200

    Make a basic install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "[alias]
+    commits = log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=iso
+    commits-by = log --author
+    contributors = shortlog -s -n
+    unstash = stash apply
+    stash-list = stash list
+    newname = branch rename
+    co = chcekout
+    pusz = push origin master
+    yolo-push = push --force
+    pul = fetch & git pull
+    dilit = push origin --delete
+    perm = update-index --chmod=+x
+    branches = show-branch -a
+    soft-reset = reset --soft
+    hard-reset = reset --hard" >> ~/.gitconfig
